cfb149acc3fd5463031b10ac3cf3aceb4e19186e
anna-orlova08 <pablopikasso918@gmail.com>
2024-12-24 00:26:30 +0300

    Add files via upload

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,34 @@
 from flask_wtf import FlaskForm  # подключение шаблона для создания собственных форм
 from wtforms import StringField, PasswordField, BooleanField, \
-    SubmitField  # подключение полей для ввода строк и паролей,расстановки галочек,кпопки входа
+    SubmitField  # подключение полей для ввода строк и паролей, расстановки галочек, кнопки входа
 from wtforms.validators import DataRequired  # подключение валидатора для проверки пустого поля
+from wtforms.validators import ValidationError, Email, EqualTo  # ValidationError-создание собственного валидатора,
+# Email-проверка соответствия почты установленным правилам, EqualTo- проверка совпадения полей формы
+from app.models import User  # подключение модели пользователей из базы данных
 
 
 class LoginForm(FlaskForm):  # создание формы для входа
-    username = StringField('Имя пользователя',
-                           validators=[DataRequired()])  # создание поля для ввода  имени пользователя
-    password = PasswordField('Пароль', validators=[DataRequired()])  # создание поля  для ввода пароля
+    username = StringField('Имя пользователя', validators=[DataRequired()])  # создание поля для ввода имени пользователя
+    password = PasswordField('Пароль', validators=[DataRequired()])  # создание поля для ввода пароля
     remember_me = BooleanField('Запомнить меня')  # галочка  запомнить меня
     submit = SubmitField('Войти')  # кнопка для входа
+
+
+class RegForm(FlaskForm):  # создание формы для регистрации
+    username = StringField('Имя пользователя', validators=[DataRequired()])  # создание поля для ввода имени пользователя
+    email = StringField('Email',validators=[DataRequired(), Email()]) # создание поля для ввода email
+    password = PasswordField('Пароль', validators=[DataRequired()])  # создание поля для ввода пароля
+    password2 = PasswordField('Повторите пароль', validators=[DataRequired(), EqualTo('password')])  # создание поля для проверки пароля
+    submit = SubmitField('Зарегистрироваться')  # кнопка для регистрации
+
+    def validate_username(self, username): # создание валидатора для проверки, что такого имени пользователя нет в базе данных
+        user = User.query.filter_by(username=username.data).first() # поиск пользователя в базе данных по указанному имени
+        if user is not None: # если пользователя удалось найти
+            raise ValidationError('Имя пользователя занято') # возврат ошибки пользователю
+
+    def validate_email(self, email): # создание валидатора для проверки, что такой почты нет в базе данных
+        user = User.query.filter_by(email=email.data).first()  # поиск пользователя в базе данных по указанной почте
+        if user is not None:  # если пользователя удалось найти
+            raise ValidationError('Пользователь с такой почтой зарегистрирован') # возврат ошибки пользователю
+
+
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
 from flask import render_template, flash, redirect  # подключение функции для генерации веб-страниц,
 # flash - функция для показа уведомлений пользователю,redirect - функция для перенаправления на другую страницу
-from app import app  # подключение веб-приложения
-from app.forms import LoginForm  # подключение формы для входа
+from app import app, db  # подключение веб-приложения и базы данных
+from app.forms import LoginForm, RegForm  # подключение формы для входа и регистрации
 from flask_login import current_user, login_user, logout_user, login_required # подключение current_user(пользователь, находящийся на сайте),
 # login_user(пользовательский загрузчик),logout_user(функция для выхода из системы), login_required(создание страниц,
 # защищённых от просмотра не вошедшими пользователями
 from app.models import User # подключение модели пользователей из базы данных
+
 
 @app.route('/')  # создание главного маршрута
 @app.route('/main')  # создание второго главного маршрута
@@ -37,7 +38,7 @@ def login():  # функция обработчик маршрута
     form = LoginForm()  # создание экземпляра формы входа
     if form.validate_on_submit():  # если пользователь нажмёт на кнопку входа,то
         user = User.query.filter_by(username=form.username.data).first() # поиск пользователя в базе данных по имени
-        if user is None or user.check_password(form.password.data): # если пользователь не найден или пароль введён неправильно
+        if user is None or not user.check_password(form.password.data): # если пользователь не найден или пароль введён неправильно
             flash('Имя пользователя или пароль неправильные') # показ уведомления пользователю о неправильных данных
             return redirect('/login') # перенаправление снова на страницу входа для повторной попытки
         login_user(user,remember=form.remember_me.data)
@@ -49,3 +50,18 @@ def login():  # функция обработчик маршрута
 def logout(): # функция обработчик страницы
     logout_user() # выход пользователя из системы
     return redirect('/main') # перенаправление на главную страницу
+
+@app.route('/register',methods=['GET',
+                              'POST'])  # маршрут страницы регистрации;метод get-показ информации пользователю, метод post-отправка данных на сервер
+def register():
+    if current_user.is_authenticated: # если пользователь уже зашёл на сайт
+        return redirect('/main') # перенаправление на главную страницу
+    form = RegForm() # создание экземпляра формы регистрации
+    if form.validate_on_submit():  # если пользователь нажмёт на кнопку регистрации, то
+        user = User(username=form.username.data,email=form.email.data) # создание пользователя по введённым данным
+        user.set_password(form.password.data) # изменение пароля пользователя
+        db.session.add(user) # добавление пользователя в базу данных
+        db.session.commit() # сохранение изменений в базе данных
+        flash('Регистрация прошла успешно')
+        return redirect('/login') # перенаправление на страницу входа
+    return render_template('register.html',title='Регистрация',form=form) # генерация страницы по шаблону register.html
